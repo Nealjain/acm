91110c21a9d363868acaa577e2fd0a8f525f428a
neal jain <neal.18191@sakec.ac.in>
2025-07-27 01:25:10 +0530

    Force new deployment: Add comment to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { Toaster } from 'react-hot-toast'
 import App from './app'
 import './index.css'
 
+// Main entry point for the SAKEC ACM application
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Toaster position="top-right" />
